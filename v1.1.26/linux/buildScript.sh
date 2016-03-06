dd87f6b2a6930e032c9c02206936fcea322959e8
Dirk Eddelbuettel <edd@debian.org>
2016-03-06 11:06:09 -0600

    Linux 64bit binary and updated build script for build 1.1.26

---
+++ v1.1.26/linux/buildScript.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# Build script for x13ashtml
+#
+# Copyright (C) 2015 - 2016 Dirk Eddelbuettel
+#
+# Released under GPL (>= 2)
+
+set -e
+set -u
+
+srctgz="https://www.census.gov/ts/x13as/unix/x13ashtmlsrc_V1.1_B26.tar.gz"
+file=$(basename ${srctgz})
+
+td=$(mktemp -d -p /tmp x13dirXXXXXX)
+cd ${td}
+wget ${srctgz}
+tar xfz ${file}
+make -f makefile.gf
+echo ""
+echo "Done"
+ls -l x13asHTML*
